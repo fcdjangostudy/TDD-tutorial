a662ec818fdb580e45618e31fc0a20db2a3691ec
GAIUS8 <gaius827@gmail.com>
2017-06-26 02:55:18 +0900

    Functional test now checks we can input a to-do item

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,18 +1,49 @@
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.test import TestCase
+import unittest
 
-from lists.views import home_page
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  #  1 request오브젝트 생성
-        response = home_page(request)  # 2 응답을 재할당
-        self.assertTrue(response.content.startswith(b'<html>'))  # 3 html테그로 시작하는지 확
-        self.assertIn(b'<title>To-Do lists</title>', response.content)  # 4 문서 제목 확인
-        self.assertTrue(response.content.endswith(b'</html>'))  # 5 끝이 html테그로 끝나는지 확인
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
